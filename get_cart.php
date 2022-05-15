9ab733c85b4b9dd9d535b6079934652097911d03
Alfon <alfonsusanto@gmail.com>
2022-05-16 02:05:03 +0700

    tambah add to cart dan get cart

---
+++ get_cart.php
@@ -0,0 +1,43 @@
+<?php
+
+    require "conn.php";
+    require "struct.php";
+    $conn = GetConnection();
+
+    $user_id = $_GET['user_id'];
+
+    $query = "SELECT * FROM tbl_cart WHERE user_id = ? ORDER BY id_cart ASC";
+    $result = $conn->prepare($query);
+    $result->execute([$user_id]);
+
+
+    $res = new ResponseGetCart();
+    $product_datas = [];
+    $total_price = 0;
+
+    while($row = $result->fetch()) {
+        $product_data = new ProductData();
+        $product_data->product_qty = $row['qty'];
+
+        $prodConn = GetConnection();
+        $query_product = "SELECT * FROM tbl_produk WHERE prod_id = ?";
+        $result_product = $prodConn->prepare($query_product);
+        $result_product->execute([$row['prod_id']]);
+        $productFetch = $result_product->fetch();
+
+        $product_data->product_id = $productFetch['prod_id'];
+        $product_data->product_name = $productFetch['prod_name'];
+        $product_data->product_type = $productFetch['type_ppr'];
+        $product_data->product_size = $productFetch['uk_ppr'];
+        $product_data->product_price = $productFetch['price'];
+
+        array_push($product_datas, $product_data);
+
+        $total_price = $total_price + ($row['qty'] * $productFetch['price']);
+    }
+
+    $res->product_data = $product_datas;
+    $res->total_price = $total_price;
+
+    echo json_encode($res);
+?>
---
+++ struct.php
@@ -0,0 +1,20 @@
+<?php 
+
+    class ResponseCart {
+        public $msg;
+    }
+
+    class ResponseGetCart {
+        public $product_data;
+        public $total_price;
+    }
+
+    class ProductData {
+        public $product_id;
+        public $product_name;
+        public $product_type;
+        public $product_size;
+        public $product_price;
+        public $product_qty;
+    }
+?>
